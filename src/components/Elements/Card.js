f7112c0cb48de51104da10dfce4fe759834a96a6
Ronald <rlopez@nicarao.agency>
2023-06-05 07:33:19 -0600

    Card component created

---
+++ src/components/Elements/Card.js
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+const Card = styled.div`
+  display: flex;
+  width: 13%;
+  height: 200px;
+  background-color: rgb(56 52 52 / 11%);
+  backdrop-filter: blur(10px);
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  align-items: center;
+  justify-content: center;
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: -10px;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    background-color: #ed6c545c;
+  }
+`;
+
+export default Card;
